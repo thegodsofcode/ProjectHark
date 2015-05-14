cd8a08abd775a05d46a44ddfd53f2f7145ab6bc5
epsooraj <epsooraj4@gmail.com>
2015-05-15 00:38:36 +0530

    Hell Yea

--- app/src/main/java/hark/bitverse/com/hark/MainActivity.java
+++ app/src/main/java/hark/bitverse/com/hark/MainActivity.java
@@ -1,9 +1,13 @@
 package hark.bitverse.com.hark;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start= (Button)findViewById(R.id.start_service);
+        Button stop = (Button) findViewById(R.id.stop_service);
+
+        start.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v)
+            {
+                startService(new Intent(getBaseContext(),MyService.class));
+            }
+
+        });
+
+        stop.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v)
+            {
+                stopService(new Intent(getBaseContext(),MyService.class));
+            }
+
+        });
     }
 
 
@@ -36,4 +61,8 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    
+
 }
+
